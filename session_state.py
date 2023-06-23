f97f092eb0ae75c3a9a70b513383870493c8a806
peardi <131191346+peardi@users.noreply.github.com>
2023-06-23 17:12:35 +0100

    Update session_state.py

--- session_state.py
+++ session_state.py
@@ -1,3 +1,9 @@
 class SessionState:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
+
+    def get(self, key):
+        return getattr(self, key, None)
+
+    def set(self, key, value):
+        setattr(self, key, value)
